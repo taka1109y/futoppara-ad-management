9c89f9ce0f00b0f9c5f351ab38d4ebab9ef2a694
taka1109y <takaaki1109t@icloud.com>
2022-01-31 01:19:56 +0900

    try except 追記

--- raspi/NFC/main.py
+++ raspi/NFC/main.py
@@ -2,42 +2,58 @@ import nfc
 import binascii
 import time
 from datetime import datetime
-
 import pymysql.cursors
+
+import sys#本番環境時消す
 
 suica=nfc.clf.RemoteTarget("212F")
 suica.sensf_req=bytearray.fromhex("0000030000")
 
+# timestamp用日付取得
 def getDate():
     dt_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(dt_now)
     return dt_now
 
+# db登録処理　引数：str(number)->ICカード番号
 def dbFunction(number):
     dt_now = getDate()
     db=pymysql.connect(host="localhost", user="dbadmin", password="dbadmin" , cursorclass=pymysql.cursors.DictCursor)
-    cursor=db.cursor()
-    cursor.execute("USE futoppara")
-    db.commit()
-    sql=('INSERT INTO kintai(user_id, datatime) VALUES((SELECT user_id from card WHERE card_num = "'+ number +'"),"'+dt_now+'")')
-    cursor.execute(sql)
-    db.commit()
-    db.close()
-    if cursor != None:
-        print('')
-    cursor.close()
 
+    try:
+        cursor=db.cursor()
+        cursor.execute("USE futoppara")
+        db.commit()
+        sql=('INSERT INTO kintai(user_id, datetime) VALUES((SELECT user_id from card WHERE card_num = "'+ number +'"),"'+dt_now+'")')
+        cursor.execute(sql)
+        db.commit()
+        if cursor != None:# 登録成功時
+            print('DB inserted! | '+dt_now)
+    except Exception as err:#例外エラー発生時
+        print(str(err))
+        pass
+    finally:
+        db.close()
+        cursor.close()
+
+# NFCスキャン処理
 def nfcScan():
-    while True:
-        with nfc.ContactlessFrontend("usb") as clf:
-            target=clf.sense(suica,iterations=3,interval=1.0)
-            while target:
-                tag=nfc.tag.activate(clf,target)
-                tag.sys=3
-                idm=binascii.hexlify(tag.idm)
-                dbFunction(str(idm.decode()))
-                time.sleep(5)
-                break
+    try:
+        while True:
+            with nfc.ContactlessFrontend("usb") as clf:
+                target=clf.sense(suica,iterations=3,interval=1.0)
+                while target:
+                    tag=nfc.tag.activate(clf,target)
+                    tag.sys=3
+                    idm=binascii.hexlify(tag.idm)
+                    dbFunction(str(idm.decode()))
+                    time.sleep(5)
+                    break
+    except Exception as err:#例外エラー発生時
+        print(str(err))
+        pass
+    except KeyboardInterrupt:
+        sys.exit(0)#本番環境時消す
+
 
 if __name__ == '__main__':
     nfcScan()
